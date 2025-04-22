4bb425ed10ead53c5ac047d06e09c029d6ee1c19
Tinyu01 <216135982@edu.vut.ac.za>
2025-04-22 10:39:50 -0700

    Create firstpage.php

---
+++ firstpage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ateu mart</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+    </head>
+
+    <style>
+
+    </style>
+    <body>
+        <!-- header -->
+         <div class="header">
+            <a><img class="logo" src="logo.png">ATEU MART</a>
+         </div>
+    </body>
+</html>
